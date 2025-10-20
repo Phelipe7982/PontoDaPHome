5976320b2b29512d6ee1528e12e451fdb23a3bab
Phelipe <ultimaterafa79@gmail.com>
2025-10-20 19:58:47 -0300

    :sparkles: feat: Criando a rota Post e Put para restaurantes

--- front_main-PontoDaPHome/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ front_main-PontoDaPHome/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -19,6 +20,9 @@ const AdministracaoRestaurantes = () => {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
                     </TableRow>
                 </TableHead>
 
@@ -26,6 +30,9 @@ const AdministracaoRestaurantes = () => {
                     {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
                         <TableCell>
                             {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link> ]
                         </TableCell>
                     </TableRow>)}
                 </TableBody>
